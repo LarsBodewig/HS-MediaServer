8ae9c5d4a3e85d42c6f39e726fdcf29b8831f500
fan2404 <fan2404@aol.com>
2019-07-02 22:11:37 +0200

    Implemented parsing of Twitter partly Extended parsing Facebook to 19 posts Removed general implementations in favor of quicker independent implementation for each source (since they're subject to change, undo when basic functionality is given)

--- src/main/java/Server/api/AbstractJson.java
+++
@@ -1,9 +0,0 @@
-package Server.api;
-
-public abstract class AbstractJson {
-
-	@Override
-	public String toString() {
-		return Api.getGson().toJson(this);
-	}
-}
--- src/main/java/Server/api/Api.java
+++ src/main/java/Server/api/Api.java
@@ -1,33 +1,5 @@
 package Server.api;
 
-import java.io.IOException;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import com.google.gson.Gson;
-
 public abstract class Api {
-
-	private static Gson gson;
-
-	public static Document connect(String url) {
-		try {
-			return Jsoup.connect(url).get();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return new Document("");
-		}
-	}
-
-	static Gson getGson() {
-		if (gson == null) {
-			gson = new Gson();
-		}
-		return gson;
-	}
 	
-	public static String arrayToJson(Object[] o) {
-		return getGson().toJson(o);
-	}
 }
